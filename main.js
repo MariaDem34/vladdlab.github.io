35e319f6e0bc2d4ab4a9c546d20da0c367e7eca2
MariaDem34 <mariademidova34@yandex.ru>
2021-03-29 19:01:06 +0300

    initial commit

--- main.js
+++ main.js
@@ -1,5 +1,31 @@
 // кажется тут должен быть код...
+let comments = [];
+loadComments();
+document.getElementById('add-comment').onclick=function()
+{
+    let commentText = document.getElementById('comment-area').nodeValue;
+    comments.push(commentText);
 
+    saveComments();
+    showComments();
+}
+
+function saveComments()
+{
+    localStorage.setItem('comments',JSON.stringify(comments));
+}
+
+function showComments()
+{
+    for (let comment of comments){
+        commentsField.insertAdjacentHTML('beforeend', <div class='comment'><p>${comment}</p></div>)
+    }
+}
+function loadComments()
+{
+    if (localStorage.getItem('comments')) comments = JSON.parse(localStorage.getItem('comments'));
+    showComments();
+}
 // Подсказка №1: Содержимое тега textarea хранится в его свойстве value
 
 // Подсказка №2: Не забывайте, что LocalStorage и SessionStorage могут хранить только строки в виде пар ключ/значение
